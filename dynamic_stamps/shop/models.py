1711ca5ab75ec023287f5e2f267e8cc9cf229bfe
Ludmila Nunes <ludmila.aysha@gmail.com>
2026-06-26 00:56:40 -0300

    feat: adiciona modelos de selo e dados iniciais

--- dynamic_stamps/shop/models.py
+++ dynamic_stamps/shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Selo(models.Model):
+    nome = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=5, decimal_places=2)
+    imagem = models.ImageField(upload_to='stamps/')
+
+    class Meta:
+        ordering = ['valor']
+
+    def __str__(self):
+        return f"{self.nome} - R$ {self.valor}"
